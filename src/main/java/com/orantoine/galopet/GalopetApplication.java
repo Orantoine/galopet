adde92b4e0e088597f2a995f7c4538b02b7cbef4
antoine <antoine.godeau@sfr.fr>
2019-12-04 21:12:42 +0100

    :tada: Initial Commit for Backend

---
+++ src/main/java/com/orantoine/galopet/GalopetApplication.java
@@ -0,0 +1,13 @@
+package com.orantoine.galopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalopetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orantoine/galopet/GalopetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orantoine.galopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
